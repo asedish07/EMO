851daf0b93c2e95916decd64b666355cbb8e7e35
김대운 <eodnseodns23@gmail.com>
2025-05-26 13:59:50 +0900

    서버 request body 입력

--- main.py
+++ main.py
@@ -1,21 +1,59 @@
-# from fastapi import FastAPI, HTTPException
-# from fastapi.responses import JSONResponse, FileResponse
-# from fastapi.middleware.cors import CORSMiddleware
-# from key import YOUR_IP
+from fastapi import FastAPI, HTTPException, Body
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from emotion_classification import emotion_classifying
+from predict_review_emotion import predict_emo, emotion_feedback
+from typing import List
+from pydantic import BaseModel
+import os
 
-# app = FastAPI()
+load_dotenv()
+YOUR_IP = os.getenv("YOUR_IP")
 
-# @app.get("/emotion")
-# async def emotion(input_diary: str):
-#   try:
-#     # 감정 분류 함수 호출
-    
+app = FastAPI()
 
-#     # 피드백 함수 호출
-    
-#   except Exception as e:
-#     raise HTTPException(status_code=500, detail={f"UnExpectedError: {e}"})
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처에서 접근 허용 (필요에 맞게 수정 가능)
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메소드 허용 (POST, GET 등)
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
-# if __name__ == "__main__":
-#   import uvicorn
-#   uvicorn.run(app="main:app", host=YOUR_IP, port=5070, reload=True)
+# request_body
+class Diary(BaseModel):
+  input_diary: str
+
+class Emotion(BaseModel):
+  emotion: List[str]
+
+@app.post("/diary")
+def emotion(input_diary: Diary):
+  try:
+    # 감정 분류 함수 호출
+    emo = ['공포', '놀람', '분노', '슬픔', '중립', '행복', '혐오']
+    emotion_prediction = emotion_classifying(input_diary.input_diary)
+    if not 0 <= emotion_prediction < len(emo):
+      raise ValueError(f"Invalid emotion index: {emotion_prediction}")
+    feedback = emotion_feedback(input_diary.input_diary)
+
+    return JSONResponse(content={"emotion": emo[emotion_prediction], "wise": feedback})
+
+  except RuntimeError as e:
+    raise HTTPException(status_code=500, detail={f"RuntimeError: {str(e)} \n It might be a problem with the prompt that user entered"})
+  except Exception as e:
+    raise HTTPException(status_code=500, detail={f"UnExpectedError: {str(e)}"})
+
+@app.post("/fortune")
+async def predict_emotion(emotion: Emotion):
+  try:
+    prediction = predict_emo(emotion.emotion)
+    return JSONResponse(content={"prediction": prediction})
+  
+  except Exception as e:
+    raise HTTPException(status_code=500, detail={f"UnExpectedError: {str(e)}"})
+
+if __name__ == "__main__":
+  import uvicorn
+  uvicorn.run(app="main:app", host=YOUR_IP, port=6600, reload=True)
---
+++ predict_review_emotion.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+from typing import List
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+
+def predict_emo(emotions: List[str]):
+  emotion = ""
+  for i in range(len(emotions)):
+    if i == (len(emotions) - 1):
+      emotion += str(emotions[i])
+    else:
+      emotion += str(emotions[i]) + ", "
+    
+  client = genai.Client(api_key=API_KEY)
+  response = client.models.generate_content(
+      model="gemini-2.0-flash",
+      config=types.GenerateContentConfig(
+          system_instruction="내 이전 날들의 감정들을 보내줄게. 내가 지금까지 하루하루 느낀 감정들을 보고 내가 오늘 느끼는 감정을 예측해줘. 짧은 한 문장으로 설명해줘."),
+      contents=emotion
+  )
+
+  return response.text
+
+def emotion_feedback(input_diary: str):
+  client = genai.Client(api_key=API_KEY)
+  response = client.models.generate_content(
+      model="gemini-2.0-flash",
+      config=types.GenerateContentConfig(
+          system_instruction="너는 심리 상담가야. 지금 너에게 나의 오늘에 대한 감정일기를 보내줄게. 응원의 말을 담아 오늘의 나에 대해 짧게 리뷰해줘."),
+      contents=input_diary
+  )
+
+  return response.text
